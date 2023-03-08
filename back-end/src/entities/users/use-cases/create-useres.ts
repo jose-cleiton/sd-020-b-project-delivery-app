d0fa487f7583dab4dfc8cbad77a83294c838fe35
jose-cleiton <cleitons835@gmail.com>
2023-03-08 10:35:50 -0300

    feat: :sparkles: implementar caso de uso para criar usuário
    O caso de uso para criar usuário foi implementado com uma implementação simples para criar uma nova entidade de usuário com base nos parâmetros da requisição fornecidos. A classe CreateUser tem um único método execute que recebe um CreateUserRequest e retorna uma Promise com uma entidade de usuário como resposta. Essa implementação atualmente define apenas as propriedades obrigatórias, id, role, created_at e updated_at serão definidas pela camada de persistência. Essa implementação é destinada a ser usada como ponto de partida para o desenvolvimento da camada de persistência.  Fecha #123

---
+++ back-end/src/entities/users/use-cases/create-useres.ts
@@ -0,0 +1,27 @@
+import Users, { usersProps } from "../domain/Users";
+
+interface CreateUserRequest { 
+  name: string;
+  email: string;
+  password: string;
+}
+
+type CreateUserResponse = Users;
+
+export class CreateUser  {
+  async execute(request: CreateUserRequest): Promise<Users> {
+    const defaultProps: usersProps = {
+      id: 0,
+      name: "",
+      email: "",
+      password: "",
+      role: "",
+      created_at: new Date(),
+      updated_at: new Date()
+    };
+    
+    const userProps = Object.assign(defaultProps, request);
+    const user = new Users(userProps);
+    return user;
+  }
+}
