bc631f4416765cb8d969cc85b6b3d1c8c531d9a5
jose-cleiton <cleitons835@gmail.com>
2023-03-08 14:54:45 -0300

    feat: :sparkles: criando as migrations

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const environment = process.env.NODE_ENV || "test";
 
@@ -11,15 +11,16 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  dialect: 'mysql',
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || "3306",
+  database: `${process.env.DB_NAME || "delivery-app"}${
+    suffix[environment] || suffix.test
+  }`,
+  username: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "123456",
+  dialect: "mysql",
   dialectOptions: {
-    timezone: 'Z',
+    timezone: "Z",
   },
   logging: false,
 };
--- back-end/src/entities/sales/use-cases/create_sales.ts
+++ back-end/src/entities/sales/use-cases/create_sales.ts
@@ -1,52 +1,53 @@
-import Sales, { SalesProps } from "../domain/Sales";
-import { CreateProduct } from "../../products/use-cases/create-products";
+// import Sales, { SalesProps } from "../domain/Sales";
+// import { CreateProduct } from "../../products/use-cases/create-products";
 
-interface CreateSaleRequest {
-  user_id: number;
-  products: {
-    product_id: number;
-    quantity: number;
-  }[];
-  delivery_address: string;
-  delivery_number: string;
-}
+// interface CreateSaleRequest {
+//   user_id: number;
+//   products: {
+//     product_id: number;
+//     quantity: number;
+//   }[];
+//   delivery_address: string;
+//   delivery_number: string;
+// }
 
-type CreateSaleResponse = Sales;
+// type CreateSaleResponse = Sales;
 
-export class CreateSale {
-  async execute({
-    user_id,
-    products,
-    delivery_address,
-    delivery_number,
-  }: CreateSaleRequest): Promise<Sales> {
-    const defaultProps: SalesProps = {
-      id: 0,
-      user_id,
-      seller_id: 0,
-      total_price: 0,
-      delivery_address,
-      delivery_number,
-      sale_date: new Date(),
-      status: "em processamento",
-    };
+// export class CreateSale {
+//   async execute({
+//     user_id,
+//     products,
+//     delivery_address,
+//     delivery_number,
+//   }: CreateSaleRequest): Promise<Sales> {
+//     const defaultProps: SalesProps = {
+//       id: 0,
+//       user_id,
+//       seller_id: 0,
+//       total_price: 0,
+//       delivery_address,
+//       delivery_number,
+//       sale_date: new Date(),
+//       status: "em processamento",
+//     };
 
-    const saleProps = Object.assign(defaultProps);
+//     const saleProps = Object.assign(defaultProps);
 
-    const sale = new Sales(saleProps);
+//     const sale = new Sales(saleProps);
 
-    let totalPrice = 0;
-    for (const { product_id, quantity } of products) {
-      const createProduct = new CreateProduct();
-      const product = await createProduct.execute({ id: product_id });
+//     let totalPrice = 0;
+//     for (const { product_id, quantity } of products) {
+//       const createProduct = new CreateProduct();
+//       const product = await createProduct.execute({ productId: product_id });
 
-      totalPrice += product.price * quantity;
 
-      sale.addProduct(product_id, quantity);
-    }
+//       totalPrice += product.price * quantity;
 
-    sale.setTotalPrice(totalPrice);
+//       sale.addProduct(product_id, quantity);
+//     }
 
-    return sale;
-  }
-}
+//     sale.setTotalPrice(totalPrice);
+
+//     return sale;
+//   }
+// }
