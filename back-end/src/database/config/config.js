50874b251e88ce2a25a2af7763759eb0d788d4c2
jose-cleiton <cleitons835@gmail.com>
2023-03-12 14:24:15 -0300

    feat: :zap: ok

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -2,13 +2,7 @@ require("dotenv").config();
 
 const environment = process.env.NODE_ENV || "test";
 
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
+
 
 const options = {
   host: process.env.DB_HOST || "localhost",
@@ -24,14 +18,4 @@ const options = {
   logging: false,
 };
 
-module.exports = {
-  development: {
-    ...options,
-  },
-  test: {
-    ...options,
-  },
-  production: {
-    ...options,
-  },
-};
+module.exports = options;
