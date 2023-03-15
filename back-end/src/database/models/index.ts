aae87f789149b19f32ad83990b88bf8667ab0a66
jose-cleiton <cleitons835@gmail.com>
2023-03-14 23:55:04 -0300

    fix: config  e index do module

--- back-end/src/database/models/index.ts
+++ back-end/src/database/models/index.ts
@@ -4,6 +4,6 @@ const { Sequelize } = require('sequelize');
 const config = require('../config/config');
 
 const { NODE_ENV } = process.env;
-const sequelize = new Sequelize(config[NODE_ENV] || config.development);
+const sequelize = new Sequelize(config || config.development);
 
 module.exports = { sequelize };
