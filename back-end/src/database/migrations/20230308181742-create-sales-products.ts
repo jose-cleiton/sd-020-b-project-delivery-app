48cd9935760245ec62e06090d366372a6cff399e
jose-cleiton <cleitons835@gmail.com>
2023-03-15 12:05:09 -0300

    feat: :sparkles: modules

--- back-end/src/database/migrations/20230308181742-create-sales-products.ts
+++ back-end/src/database/migrations/20230308181742-create-sales-products.ts
@@ -2,6 +2,6 @@ import { QueryInterface } from 'sequelize';
 import { salesProductTable } from '../table/salesProductTable';
 
 export const up = async (queryInterface: QueryInterface) => {
-  await queryInterface.createTable('sales-products', salesProductTable);
+  await queryInterface.createTable('sales_products', salesProductTable);
 };
 
